c82db81b049bb0c6d08f2b02ac44ba9f900371bc
Bracco Fabricio <braccof.0601@gmail.com>
2025-09-07 14:12:02 -0300

    feat: add sample code for methods

---
+++ 01-fundamentals/Methods.java
@@ -0,0 +1,16 @@
+class Methods {
+    // A method that adds two integers and returns the result
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // A method that prints "Hello, World!"
+    public static void hello() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(5, 10)); // Output: 15
+        hello(); // Output: Hello, World!
+    }
+}
